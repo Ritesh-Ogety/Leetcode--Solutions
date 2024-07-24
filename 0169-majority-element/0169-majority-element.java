00fe4cf6d6beac95a05e79fe501cd22335c19223
Ritesh-Ogety <161813686+Ritesh-Ogety@users.noreply.github.com>
2024-07-24 14:23:50 +0530

    Time: 2 ms (75.21%), Space: 53.5 MB (6.8%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int n = nums.length;
+
+        int majorityElement = -1;  // Initialize to a value indicating no majority element
+        int maxCount = 0;
+
+        for (int i = 0; i < n; i++) {
+            boolean flag = false;
+            int count = 1;
+
+            for (int j = i + 1; j < n; j++) {
+                if (nums[i] == nums[j]) {
+                    flag = true;
+                    break;
+                }
+            }
+
+            if (flag) {
+                continue;
+            } else {
+                for (int j = 0; j <= i; j++) {
+                    if (nums[i] == nums[j]) {
+                        count++;
+                    }
+                }
+            }
+
+            if (count > maxCount) {
+                maxCount = count;
+                majorityElement = nums[i];
+            }
+        }
+
+        // Check if the majority element appears more than n/2 times
+        if (maxCount > n / 2) {
+            return majorityElement;
+        } else {
+            return -1;
+        }
+    }
+
+}
