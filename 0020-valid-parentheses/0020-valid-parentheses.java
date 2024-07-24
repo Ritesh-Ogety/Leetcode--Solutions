0bb3a44ace44c615751571fd60b8b532581a3bc9
Ritesh-Ogety <161813686+Ritesh-Ogety@users.noreply.github.com>
2024-07-24 14:25:56 +0530

    Time: 2 ms (53.37%), Space: 41.5 MB (42.22%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,35 @@
+class Solution 
+{
+    public boolean checkPair(char val1,char val2)
+    {
+        return (val1=='('&&val2==')')||(val1=='['&&val2==']')||(val1=='{'&&val2=='}');
+    }
+    public boolean isValid(String s) 
+    {
+        Stack <Character> x=new Stack<>();
+        for(int i=0;i<s.length();i++)
+        {
+            char pchar=s.charAt(i);
+            if(pchar=='('||pchar=='['||pchar=='{')
+            {
+                x.push(pchar);
+            }
+            else
+            {
+                if(x.isEmpty() || !checkPair(x.peek(),pchar))
+                {
+                    return false;
+                }
+                x.pop();
+            }
+        }
+        return x.isEmpty();
+    }
+
+
+    public static void main(String[] args)
+    {
+        Solution s1=new Solution();
+        System.out.println(s1.isValid("()"));
+    }
+}
