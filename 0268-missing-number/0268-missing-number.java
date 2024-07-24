30c2e9782d7fceb97ca94e9c0e0609dde785e888
Ritesh-Ogety <161813686+Ritesh-Ogety@users.noreply.github.com>
2024-07-24 14:11:34 +0530

    Time: 5 ms (26.41%), Space: 45.4 MB (23.51%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,32 @@
+
+class Solution 
+{
+    public int missingNumber(int[] nums) 
+    {
+        int val;
+        Arrays.sort(nums);
+
+        if(nums[0]!=0)
+        {
+            return 0;
+        }
+        for(int i=0;i<nums.length-1;i++)
+        {
+            if(nums[i+1]-nums[i]>1)
+            {
+                return nums[i]+1;
+                
+            }
+        }
+        return nums[nums.length-1]+1;
+    }
+
+
+    public static void main(String[] args)
+    {
+        int [] arr={3,0,1};
+        Solution s1=new Solution();
+        int res=s1.missingNumber(arr);
+        System.out.println(res);
+    }
+}
