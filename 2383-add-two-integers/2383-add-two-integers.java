183f7aa6b9d51e59abb6836f83bfcdf4d6e4acc6
Ritesh-Ogety <161813686+Ritesh-Ogety@users.noreply.github.com>
2024-07-24 13:57:13 +0530

    Time: 0 ms (100%), Space: 40.6 MB (17.79%) - LeetHub

---
+++ 2383-add-two-integers/2383-add-two-integers.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int sum(int num1, int num2) 
+    {
+        return num1+num2;
+    }
+    public static void main(String[] args)
+    {
+        Solution s1=new Solution();
+        System.out.println(s1.sum(12,5));
+    }
+}
