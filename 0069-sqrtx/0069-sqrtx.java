21127685fe7fda9397a4c3247f8da6e436e50d38
Ritesh-Ogety <161813686+Ritesh-Ogety@users.noreply.github.com>
2024-07-24 13:58:11 +0530

    Time: 0 ms (100%), Space: 41.3 MB (5.45%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int mySqrt(int x) 
+    {
+        return (int)Math.sqrt(x);
+    }
+
+    public static void main(String[] args)
+    {
+        Solution s1=new Solution();
+        System.out.println(s1.mySqrt(4));
+    }
+}
