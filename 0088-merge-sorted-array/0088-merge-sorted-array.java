918301d12064e351e70cefc4af38218386347e13
O.Ritesh <161813686+Ritesh-Ogety@users.noreply.github.com>
2025-02-03 14:41:46 +0530

    Time: 15 ms (2.78%), Space: 42.7 MB (8.4%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,62 +1,38 @@
 class Solution 
 {
-    public void merge(int[] nums1, int m, int[] nums2, int n) 
+    public void merge(int[] nums1, int m, int[] nums2, int n)
     {
-        int [] res=new int[nums1.length];
-        int k=0;
         int i=0;
         int j=0;
-        
+        int k=0;
+        int [] res=new int[nums1.length];
+
         while(i<m && j<n)
         {
             if(nums1[i]<=nums2[j])
             {
-                res[k]=nums1[i];
-                i++;
-                k++;
+                res[k++]=nums1[i++];
             }
             else
             {
-                res[k]=nums2[j];
-                j++;
-                k++;
+                res[k++]=nums2[j++];
             }
         }
-
-
-            while(i<m)
-            {
-                res[k]=nums1[i];
-                i++;
-                k++;
-            }
-            while(j<n)
-            {
-                res[k]=nums2[j];
-                j++;
-                k++;
-            }
-
+        while(i<m)
+        {
+            res[k++]=nums1[i++];
+        }
+        while(j<n)
+        {
+            res[k++]=nums2[j++];
+        }
         for(i=0;i<m+n;i++)
         {
             nums1[i]=res[i];
-        }   
-        
+        }
         for(i=0;i<m+n;i++)
         {
-            System.out.print(nums1[i]+" ");
-        }
-    }
-
-
-
-    public static void main(String[] args)
-    {
-        int [] arr1={1,2,3,0,0,0};
-        int m=3;
-        int [] arr2={2,5,6};
-        int n=3;
-        Solution s1=new Solution();
-        s1.merge(arr1,m,arr2,n);
+            System.out.print(nums1[i]+ " ");
+        }    
     }
 }
